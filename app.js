0335401924f5d4d8f17650afe2536e6a21c91f33
leandro joel ramos <leandrojoelramos7@gmail.com>
2022-05-26 06:51:37 -0300

    añadiendo archivos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const public = path.join(__dirname, './public');
+app.use(express.static(public))
+
+app.get("/",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+})
+app.get("/login",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/login.html"));
+})
+app.get("/register",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/registro.html"));
+})
+
+
+app.listen(8080,()=>{
+    console.log('¡¡ esta vivo !!');
+}) 
+
